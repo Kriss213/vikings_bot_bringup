551c423e06dc06738af45064f5b338fc69fa2414
Kriss213 <kriss.vasermanslv@gmail.com>
2025-04-08 13:01:13 +0300

    rewrite

--- README.md
+++ README.md
@@ -4,80 +4,43 @@ This package conatains files to bringup nodes related to Vikings Bot project.
 
 ## Setup
 
-### Setup Udev rules for joystick:
-```
-sudo bash udev/create_udev_rules.sh
-```
-Rules can be deleted by executing ```delete_udev_rules.sh``` script the same way.
 
 ### Install dependencies:
 ```
 rosdep install -y --from-paths src --ignore-src
 ```
 
-## Launch files
-
-__This package contains multiple launch files. Main ones are:__
-
-__Real robot:__
-
-* ```real_robot_navigation_+_cam.launch.py``` - Launch all components on real robot.
-
-* Other launch files for real robot launch parts of system, e.g. camera, localizaiton, navigation etc.
-
-__Simulation:__
-
-* ```start_simulation.launch.xml``` - Start Gazebo for simulation.
-* ```bringup_first_bot.launch.xml``` - Simulate 1 robot.
-* ```bringup_two_bots.launch.xml``` - Simulate 2 robots.
-
-
 
 ## Simulation
-
-### Configuration
-To enable lidar / depth cam filtering, set parameters in `bringup_first_bot.launch.xml` (only for 1 robot):
-```
-<let name='filter_lidar_param' value='true'/>
-<let name='filter_depth_cam_param' value='true'/>
-<let name='use_lidar_param' value='true'/>
-<let name='use_depth_cam_param' value='true'/>
-```
 ### Start simulation
+
 __Launch gazebo world:__
 
 ```
 ros2 launch vikings_bot_bringup start_simulation.launch.xml
 ```
 
-__A) To spawn one robot, execute:__
+__To spawn robot execute:__
 ```
-ros2 launch vikings_bot_bringup bringup_first_bot.launch.xml
+ros2 launch vikings_bot_bringup bringup_vikings_bot.launch.py
 ```
 
-__B) To spawn two robots, execute:__
+__To spawn another robot execute:__
 ```
-ros2 launch vikings_bot_bringup bringup_two_bots.launch.xml
+ros2 launch vikings_bot_bringup bringup_vikings_bot.launch.py robot_nr:=2
 ```
 In Rviz enable second robot visualization.
 
-__To move robots with keypad, execute:__
+__To move robots with keypad execute:__
 
 Vikings Bot 1:
 ```
 ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r cmd_vel:=vikings_bot_1/cmd_vel
 ```
 
-Vikings Bot 2:
+For different numbered robots:
 ```
-ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r cmd_vel:=vikings_bot_2/cmd_vel
+ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r cmd_vel:=vikings_bot_{robot_nr}/cmd_vel
 ```
 
 __To test Nav2, in Rviz us `2D Goal Pose` button to send goal to robot.__
-
-
-In case something is not working, let me know!
-
-
-
-
---
+++ launch/bringup_n_bots.launch.py
@@ -0,0 +1,130 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+def launch_setup(context, *args, **kwargs):
+    nodes = []
+    
+    # map server
+    map_server_node = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('vikings_bot_map_server'),
+                'launch',
+                'map_server.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'use_rviz': 'False',
+            'map_file': 'simulation_map.yaml',
+        }.items()
+    )
+    nodes.append(map_server_node)
+
+    robot_count = int(LaunchConfiguration('robot_count').perform(context))
+
+
+    for i in range(1,robot_count+1):
+        namespace = f'vikings_bot_{i}'
+        
+        # Spawn robot in gazebo
+        spawn_node = IncludeLaunchDescription(
+            AnyLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('vikings_bot_gazebo'),
+                    'launch',
+                    'spawn.launch.xml'
+                ])
+            ]),
+            launch_arguments={
+                'vikings_bot_name': namespace,
+                #'robot_file': 'vikings_bot.xacro',
+                'use_sim': 'true',
+                'x_spawn': '-4', #'11.0',
+                'y_spawn': str(-2+(i-1)*1.2), #str(-7+(2*(i-1))),
+                'z_spawn': '0.2',
+                'roll_spawn': '0.0',
+                'pitch_spawn': '0.0',
+                'yaw_spawn': '3.14'
+            }.items()
+        )
+
+        
+
+        # localization
+        localization_node = IncludeLaunchDescription(
+            AnyLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('vikings_bot_localization_server'),
+                    'launch',
+                    'spawn_localization.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'vikings_bot_name': namespace,
+                'x_spawn': '-4',# 11.0',
+                'y_spawn': str(-2+(i-1)*1.2),#str(-7+(2*(i-1))),
+                'yaw_spawn': '3.14',
+            }.items()
+        )
+
+        # path planner server
+        path_planner_node = IncludeLaunchDescription(
+            AnyLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('vikings_bot_path_planner_server'),
+                    'launch',
+                    'spawn_path_planner.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'namespace': namespace,
+            }.items()
+        )
+
+
+        nodes.append(spawn_node)
+        nodes.append(localization_node)
+        nodes.append(path_planner_node)
+
+    # rviz node
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name=f'rviz2_{namespace}',
+        output='screen',
+        arguments=['-d', PathJoinSubstitution([
+            FindPackageShare('vikings_bot_bringup'),
+            'rviz',
+            'vikings_bot.rviz'
+        ])],
+        parameters=[{
+            'use_sim_time': True,
+        }]
+    )
+    
+
+    return [
+        rviz_node,
+    ]+nodes
+
+def generate_launch_description():
+    """
+    Launch N robots in simulation.
+    """
+    
+    #Robot count argument
+    robot_count_arg = DeclareLaunchArgument(
+        'robot_count',
+        default_value='1',
+        description='Number of robots to spawn'
+    )
+    
+
+    return LaunchDescription([
+        robot_count_arg,
+        OpaqueFunction(function=launch_setup)
+    ])
